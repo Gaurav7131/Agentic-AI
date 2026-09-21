df0d9cdfb622815c0a49fb5f507410a43cf41406
Gaurav7131 <thakaregaurav7131@gmail.com>
2026-09-21 15:46:32 +0530

    Add Vector embedding pom,AiConfig Fix Changes,Add .TXT as a private resouces in src/resources/docs/**

--- spring-ai-groq/src/main/java/com/example/spring_ai_groq/Config/AiConfig.java
+++ spring-ai-groq/src/main/java/com/example/spring_ai_groq/Config/AiConfig.java
@@ -1,12 +1,5 @@
-package com.example.spring_ai_groq.Config;
+package com.example.spring_ai_groq.config;
 
-import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
-import org.springframework.context.annotation.Bean;
+public class Aiconfig {
 
-public class AiConfig {
-    @Bean
-    public InMemoryChatMemoryRepository chatMemoryRepository() {
-        // In-memory rep0;swapin inredis repositories
-        return new InMemoryChatMemoryRepository();
-    }
 }
